384e6ac48f34374aa8b4c7d5aff9b4e0c7918d01
James T <jtara@tuta.io>
2017-08-27 19:52:58 -0700

    update zip folders script; note: if file path is too long, it'll fail

--- zip_folders.py
+++ zip_folders.py
@@ -4,8 +4,9 @@ import os
 
 
 @click.command()
-@click.argument('folder', type=click.Path())
-def zip_folders(folder):
+@click.argument('folder', type=click.Path(), default=os.getcwd())
+@click.option('-v', type=click.BOOL, is_flag=True)  # verbose, print extra info
+def zip_folders(folder, v):
     folder = os.path.abspath(folder)
     # iterate over all the folders & files
     for f in os.listdir(folder):
@@ -13,10 +14,15 @@ def zip_folders(folder):
         # this item is a file, not a folder
         if not os.path.isdir(current_folder):
             continue
+
+        if v:
+            print("ZIPPING FOLDER: {}".format(current_folder))
         # open a zip file (to write to)
         with zipfile.ZipFile(current_folder + ".zip", 'w') as z_file:
             # write each file or folder in the current_folder
             for file in os.listdir(current_folder):
+                if v:
+                    print('zipping: {}'.format(file))
                 z_file.write(os.path.join(current_folder, file))
 
 
