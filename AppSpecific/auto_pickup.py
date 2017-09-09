869f71e56080e053a3f0c2406eadce865a9ea5cd
James T <jtara@tuta.io>
2017-09-08 18:12:14 -0700

    improve oop design of auto_pickup script and add features (more than just pickup script now)

--- AppSpecific/auto_pickup.py
+++ AppSpecific/auto_pickup.py
@@ -5,17 +5,24 @@ import threading
 
 
 class Action:
-    def __init__(self):
+    def __init__(self, hotkey):
         """
-        Interface for can action
+        Interface for an action
         """
+        self.hotkey = hotkey
         self.enabled = False
         self.terminate = False
         self.delay_between_picksups = 0.5
 
         self.thread = threading.Thread(target=self.start)
         self.thread.start()
-
+        self.create_hotkey()
+		
+    def create_hotkey(self):
+        keyboard.add_hotkey(
+            hotkey=self.hotkey,
+            callback=self.toggle_enabled)
+                            
     def start(self):
         self._run_repeatedly()
 
@@ -38,28 +45,28 @@ class Action:
         raise Exception("Not implemented yet")
     
     def toggle_enabled(self):
-        print('toggle enabled')
         self.enabled = not self.enabled
+        print(__class__.__name__ + str(self.enabled))
         
 
 class Pickup(Action):
-    def __init__(self):
+    def __init__(self, hotkey='F2'):
         """
         Presses f repeatedly which picks up loot
         in a certain online game
         """
-        super().__init__()
+        super().__init__(hotkey=hotkey)
         
     def do_action(self):
         keyboard.press_and_release('f')
 
 
 class Digger(Action):
-    def __init__(self):
+    def __init__(self, hotkey='F3'):
         """
         Digs dirt in a certain online game
         """
-        super().__init__()
+        super().__init__(hotkey=hotkey)
 
     def do_action(self):
         mouse.press(button='right')
@@ -69,19 +76,34 @@ class Digger(Action):
         time.sleep(3)  # approx time to dig in game
 
 
-class Runner:
-    def __init__(self):
-        self.pickup = Pickup()
-        self.digger = Digger()
-        self.start()
-        
-    def start(self):
-        keyboard.add_hotkey(hotkey='F2',
-                            callback=self.pickup.toggle_enabled)
-        keyboard.add_hotkey(hotkey='F3',
-                            callback=self.digger.toggle_enabled)
+class Walker(Action):
+    def __init__(self, hotkey='F4'):
+        """
+        Walks forward until disabled
+        """
+        super().__init__(hotkey=hotkey)
+
+    def do_action(self):
+        print('do_action')
+        if self.enabled:
+            keyboard.press('w')
+        else:
+            keyboard.release('w')
+
+    def _run_repeatedly(self):
+        """Don't need to do this for this macro"""
+        pass
+
+    def toggle_enabled(self):
+        super().toggle_enabled()
+        self.do_action()
     
 
 if __name__ == "__main__":
     # start the program
-    Runner()
+    Pickup()
+    Digger()
+    Walker()
+    # block until program exit (KeyBoardInterrupt) ctrl+c
+    while True:
+        time.sleep(10)
