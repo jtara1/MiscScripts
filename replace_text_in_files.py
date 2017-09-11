453320d11ccd8970c6fb7aa537e05e4d365eacb4
James T <jtara@tuta.io>
2017-09-10 18:41:11 -0700

    fix text replacer

--- replace_text_in_files.py
+++ replace_text_in_files.py
@@ -1,19 +1,28 @@
 import click
 import os
+from glob import glob
 
 
 @click.command()
-@click.argument('replace_text')
-@click.argument('search_text')
+@click.argument('search_text', type=click.STRING)
+@click.argument('replace_text', type=click.STRING)
 @click.argument('folder', default=os.getcwd())
 def main(folder, search_text, replace_text):
+    print('-' * 20)
+    print('searching for: ' + search_text)
+    print('searching for: ' + replace_text)
+
     folder = os.path.abspath(folder)
-    for file in os.listdir(folder):
+    for file in glob(os.path.join(folder, '*')):
+        if os.path.isdir(file):
+            continue
+
+        print('looking through: ' + str(file))
         with open(file, 'r+') as f:
             text = f.read()
             updated_text = text.replace(search_text, replace_text)
-            text.write(updated_text)
-
+            f.seek(0)
+            f.write(updated_text)
 
 if __name__ == '__main__':
     main()
