b813cfc48b5b43ba0839a1978abd56dd1f2d5839
James T <jtara@tuta.io>
2017-11-20 14:19:56 -0800

    fix formula to jflap xml converter

--- formula_to_jflap_grammar.py
+++ formula_to_jflap_grammar.py
@@ -1,7 +1,7 @@
 import click
 import re
 import os
-
+import functools
 
 __doc__ = """
 Convert a libre office forumla into an XML .jff file accepted by JFLAP
@@ -13,59 +13,59 @@ symbol.
 RHS_DIVIDER = '"|"'  # == "\"|\""
 NEWLINE = "newline"
 
+
 @click.command()
-@click.argument("formula_text")
-@click.option("output_path", help="Include full file path to file with .jff "
-                                  "file is created within script",
-              default=os.path.join(os.getcwd(), "grammar.jff"))
-def main(formula_text, output_path):
+@click.argument("formula_text", nargs=-1)
+def main(formula_text):
+    formula = functools.reduce(lambda a, b: a + b, formula_text)
+    output_path = os.path.join(os.getcwd(), "grammar.jff")
+
+    xml_out = convert(formula)
     with open(output_path, 'w') as f:
-        f.write(convert(formula_text))
+        f.write(xml_out)
+    print(xml_out)
 
 
 def convert(formula_text):
-    re.sub(" ", "", formula_text)
-    xml_output = """
-        <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    """Convert a libre office formula to an XML .jff accepted by JFLAP
+    
+    :param formula_text: the raw text used to build the formula
+    :return: a string of XML
+    """
+    # rm whitespace characters ('\n', ' ', '\t', etc.)
+    formula_text = re.sub("\s", "", formula_text)
+    xml_output = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
         <!--Created with JFLAP 6.4.--><structure>
         <type>grammar</type>
-        <!--The list of productions.-->"""
+        <!--The list of productions.-->\n"""
 
-    for rule in formula_text.split("newline"):
-        xml_output += "<production>"
+    for rule in formula_text.split(NEWLINE):
         variable = ""
         # determine the variable of the rule
         for index, letter in enumerate(rule, start=0):
             # if production rule arrow encountered,
             # and the '-' char is not last character of rule
-            if letter == '-' and len(letter) - 1 != index \
-                    and letter[index+1] == ">":
+            if letter == '-' and len(rule) - 1 != index \
+                    and rule[index+1] == ">":
                 index += 2
                 break
             variable += letter
 
-        rule_done = False
         # determine the right hand side of the rule
-        # for index2, letter in enumerate(rule[index:], start=index):
         for right_element in rule[index:].split(RHS_DIVIDER):
             right_output = ""
             for index2, letter in enumerate(right_element, start=0):
-                if right_element[index2:] == NEWLINE:
-                    rule_done = True
-                    break
                 right_output += letter
 
-            if rule_done:
-                break
-
-            xml_output += "<left>{}</left>".format(variable)
-            xml_output += "<right>{}</right>".format(right_output)
-
-        xml_output += "</production"
+            xml_output += "\t<production>\n" \
+                          "\t\t<left>{}</left>\n" \
+                          "\t\t<right>{}</right>\n" \
+                          "\t</production>\n".format(variable, right_output)
 
     # done reading all production rules
     xml_output += "</structure>"
     return xml_output
+
 
 if __name__ == '__main__':
     test_input = """
