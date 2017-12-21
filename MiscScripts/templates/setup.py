6eddc70cc526eb2fcbd6203e36022455e1b2c3c4
James T <jtara@tuta.io>
2017-12-21 14:14:06 -0800

    add template setup.py

---
+++ MiscScripts/templates/setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+import re
+from os.path import join, dirname, basename
+
+
+version = '0.1.0'
+__file_path = dirname(__file__)
+# get module name from parent folder name
+# assumes the parent folder (repository name) is the same as the module name
+module_name = basename(dirname(__file_path))
+github_user = 'jtara1'
+github_url = 'https://github.com/{}/{}'.format(github_user, module_name)
+with open(join(__file_path, 'README.rst')) as f:
+    readme = f.read()
+
+
+def get_install_requirements():
+    requirements = []
+    try:
+        with open(join(__file_path, 'requirements.txt'), 'r') as req_file:
+            for line in req_file:
+                requirements.append(re.sub("\s", "", line))
+    except (FileExistsError, FileNotFoundError):
+        pass
+    return requirements
+
+
+setup(name=module_name,
+      packages=[module_name],
+      version=version,
+      description='Wrapper for pymediainfo that retrieves information such as'
+                  'media type, duration, resolution, etc. from media files',
+      long_description=readme,
+      author='James T',
+      author_email='jtara@tuta.io',
+      url=github_url,
+      download_url='{github_url}/archive/{version}.tar.gz'
+      .format(github_url=github_url, version=version),
+      keywords=[],
+      install_requires=get_install_requirements(),
+      classifiers=[]
+      )
