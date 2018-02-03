ebaed58f59f3bf141b16d9b14ff3eacbeab5d21b
James T <jtara@tuta.io>
2018-02-02 18:02:49 -0800

    add: .bashrc with my aliases and env vars for python, venv, git

---
+++ misc_scripts/.bashrc.sh
@@ -0,0 +1,13 @@
+export GITHUB=/home/j/_Github-Projects
+export SCRIPTS=$GITHUB/misc_scripts
+export GOPATH=$GITHUB/go
+export GOBIN=/opt/go-bin
+
+alias pip="pip2"
+alias pip3="pip3.5"
+alias python3="/usr/bin/python3.5"
+alias pypi="$GITHUB/misc_scripts/DevOps/python_setup_sdist.sh"
+alias tagnext="/home/j/_Github-Projects/misc_scripts/misc_scripts/DevOps/create_and_push_next_tag.sh"
+alias ytdl="youtube-dl"
+alias mkvenv="virtualenv --python=python3 venv"
+alias venv=". ./venv/bin/activate"
