d812d1e9b5096a9e3d0cd14abd6178389b80e759
James T <jtara@tuta.io>
2018-02-07 11:49:50 -0800

    [wip] search text in files updated and fixed

--- misc_scripts/search_text_in_files.py
+++ misc_scripts/search_text_in_files.py
@@ -24,61 +24,37 @@ def main(path, search_text, stand_alone):
         return char == ' ' or char == '\n' or char == '\t'
 
     path = os.path.abspath(path)
+    output = {}
     for file in glob(os.path.join(path, '*')):
+        output[file] = []
         # skip folders
         if os.path.isdir(file):
             continue
 
         print('looking through: ' + str(file))
         # get text in file
-        with open(file, 'r') as f:
-            text = f.read()
+        try:
+            with open(file, 'r') as f:
+                text = f.read()
+        except UnicodeDecodeError:
+            continue
 
         # contains the tuples of the start and the end of each match
         # e.g.: [(0, 1), (10, 11)]
         indices = []
         start = 0
         while start < len(text):
-            # matches = re.search(pattern, text)
             matches = pattern.search(text, start)
-            # matches = re.findall(pattern, text)
             if matches:
                 print(matches.group(0))
-                # print(matches.groups())
-                # print(matches.group(1))
-                end = matches.end(0)
+                start = matches.start(1)
                 indices.append(start)
-                start = end
+                start += len(search_text)
             else:
                 break
 
-        # print(indices)
-        # print([text[a:b+1] for a, b in indices])
-        # print([text[i:len(search_text) + i + 1] for i in indices])
-        # if not stand_alone:
-        #     text = text.replace(search_text, replace_text)
-        # else:
-        #     index = 0
-        #
-        #     # search for occurances of search_text found in text
-        #     while index < len(text) - len(search_text):
-        #         index = text.find(search_text, index)
-        #         if index == -1:
-        #             break  # search_text not found
-        #
-        #         # index to left of search text
-        #         l_index = index - 1
-        #         # index to right of s. text
-        #         r_index = index + len(search_text)
-        #
-        #         # left side and right side is clear
-        #         # (search_text is standalone)
-        #         if (l_index < 0 or is_whitespace(text[l_index])) \
-        #                 and (r_index >= len(text) or is_whitespace(
-        #                     text[r_index])):
-        #             text = text[:index] + replace_text + text[r_index:]
-        #         else:
-        #             index = r_index  # becomes the starting point to search
+        
+        # print([text[a:len(search_text) + a] for a in indices])
 
 
 if __name__ == '__main__':
