55714bf61d69d0b7694b5818779dcd6bf91169b8
James T <jtara@tuta.io>
2018-05-09 14:39:12 -0700

    chg: allow count_lines to ignore a dir or file

--- misc_scripts/DevOps/count_lines.py
+++ misc_scripts/DevOps/count_lines.py
@@ -7,13 +7,17 @@ import os
 @click.argument('file_path', type=click.Path())
 @click.argument('file_ext', type=click.STRING)
 @click.option('-r', help='recursive', is_flag=True, default=False)
-def main(file_path, file_ext, r):
+@click.option('-i', help='ignore this directory or file if i in the path')
+def main(file_path, file_ext, r, i):
     lines = 0
     files = 0
     all_files = glob(os.path.join(file_path, '**/*.'+file_ext), recursive=r) if r \
         else glob(os.path.join(file_path, '*.'+file_ext), recursive=r)
 
     for file in all_files:
+        print(file)
+        if i in file:
+            continue
         files += 1
         for _ in open(file, 'r'):
             lines += 1
